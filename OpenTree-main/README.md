43b7be59f345a0de1259a8eb99e503bdb4962a28
SingABrightSong <adrian.halls@outlook.com>
2022-06-18 07:00:14 -0600

    Add files via upload

---
+++ OpenTree-main/README.md
@@ -0,0 +1,27 @@
+# OpenTree
+
+A modified tech tree with clearer choices and development. Supports CTT and mod parts.
+
+### Features
+
+* Increased player freedom with more options available from the start: crewed/uncrewed rovers, crewed rockets, probes, planes and drones.
+* Entry level tech offers the bare minimum to provide a fun and interesting challenge.
+* Tech development is based on finer levels of control and ease of use - not just increasing power.
+* The parts contained in each tech either work well together or are related in function. No more illogical groupings.
+
+### Specifics
+
+* Choose between crewed or uncrewed tech in the new game options.
+* Launch your first plane/drone with solid fuel boosters, and your first rocket/probe with mono-propellant... or go for a drive around KSC with a rover.
+* Earlier access to docking ports, RCS, cargo bays, ISRU and turbojet engines.
+* Points of entry to Mk2, Mk3, 2.5m and 3.75m parts from both rocketry and aviation lines.
+* Players are encouraged to lower the science rewards rate for a greater challenge - OpenTree has been balance tested down to 30% science rewards.
+ 
+To install simply extract to your KSP installation directory. When installing for the first time, please start a new game.
+
+![CTT](https://i.imgur.com/pOpdwHo.png)
+
+Supports Community Tech Tree (download separately) and mod parts.
+
+![tree](https://i.imgur.com/ji1gEjk.png)
+
---
+++ OpenTree-main/source/OpenTree.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+
+namespace OpenTree {
+    public class OpenTreeSettings : GameParameters.CustomParameterNode {
+        public override GameParameters.GameMode GameMode { get { return GameParameters.GameMode.CAREER | GameParameters.GameMode.SCIENCE; } }
+        public override string Section { get { return "OpenTree"; } }
+        public override string DisplaySection { get { return "OpenTree"; } }
+        public override bool HasPresets { get { return false; } }
+        public override int SectionOrder { get { return 0; } }
+        public override string Title { get { return "#autoLOC_190662"; } }
+        [GameParameters.CustomParameterUI("#autoLOC_900350", newGameOnly = true)]
+        public string start = "Uncrewed";
+        public override IList ValidValues(MemberInfo member) => new string[2] { "Uncrewed", "Crewed" };
+    }
+    [KSPAddon(KSPAddon.Startup.SpaceCentre, false)]
+    public class OpenTreeSetup : MonoBehaviour {
+        public void Start() {
+            if (HighLogic.CurrentGame.Mode == Game.Modes.CAREER || HighLogic.CurrentGame.Mode == Game.Modes.SCIENCE_SANDBOX) {
+                GameEvents.OnTechnologyResearched.Add(TechResearched);
+                string startTech = HighLogic.CurrentGame.Parameters.CustomParams<OpenTreeSettings>().start.ToLower() + "Tech";
+                if (ResearchAndDevelopment.Instance.GetTechState(startTech) == null)
+                    ResearchAndDevelopment.Instance.UnlockProtoTechNode(new ProtoTechNode { scienceCost = 5, techID = startTech });
+            }
+        }
+        public void OnDisable() => GameEvents.OnTechnologyResearched.Remove(TechResearched);
+        private void TechResearched(GameEvents.HostTargetAction<RDTech, RDTech.OperationResult> action) {
+            if (action.host.techID == "structuralII" && action.target == RDTech.OperationResult.Successful) {
+                ResearchAndDevelopment.Instance.UnlockProtoTechNode(new ProtoTechNode { scienceCost = 1, techID = "generalConstruction" });
+            }
+        }
+    }
+}
